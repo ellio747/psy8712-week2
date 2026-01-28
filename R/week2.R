d37071431b8aea821a66eab33edc9966acd0aa71
Ben Elliott <ellio747@umn.edu>
2026-01-28 09:48:02 -0600

    assignment complete

--- R/week2.R
+++ R/week2.R
@@ -6,4 +6,6 @@ rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "N"), labels = c("Male
 # Analysis
 mean(rt_df$rt)
 rt_f_df <- subset(rt_df, gender == "Female")
-hist()
+hist(rt_f_df$rt)
+datasets <- list(rt_df, rt_f_df)
+datasets[[1]]$rt
